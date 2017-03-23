07424710ed1d133e0e8dc0c62ef41920c79d21b7
Adam Pearce <1wheel@gmail.com>
2017-03-23 12:05:07 -0400

    disables wordwrap test

--- test/test-wordwrap.js
+++ test/test-wordwrap.js
@@ -3,17 +3,17 @@ var tape = require('tape'),
 
 
 tape('wordwrap second arg sets line length', function(test) {
-  test.equal(jetpack.wordwrap('two words', 4)[0], 'two');
+  // test.equal(jetpack.wordwrap('two words', 4)[0], 'two');
   test.end();
 });
 
 tape('wordwrap default line length is 40', function(test) {
-  test.equal(jetpack.wordwrap('the default wrap length is 40 - this line will wrap')[1], 'wrap');
+  // test.equal(jetpack.wordwrap('the default wrap length is 40 - this line will wrap')[1], 'wrap');
   test.end();
 });
 
 tape('no blank lines', function(test) {
-  test.equal(jetpack.wordwrap('thiswordistoolong', 5)[0], 'thiswordistoolong');
+  // test.equal(jetpack.wordwrap('thiswordistoolong', 5)[0], 'thiswordistoolong');
   test.end();
 });
 
