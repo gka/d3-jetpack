4b3e672a435d7d2c175bf7c720036efcbb19e2c3
Gregor Aisch <gka@users.noreply.github.com>
2017-09-25 00:38:52 +0200

    no ugly comments

--- src/translate-selection.js
+++ src/translate-selection.js
@@ -1,5 +1,4 @@
 export default function(xy, dim) {
-  // console.log('NODE', this.node());
   return this.node().getBBox ?
     this.attr('transform', function(d,i) {
       var p = typeof xy == 'function' ? xy.call(this, d,i) : xy;
