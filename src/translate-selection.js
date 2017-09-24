f30a391aa0c594d78b94cf0ad0010c8a728b83cb
Gregor Aisch <gka@users.noreply.github.com>
2017-09-25 00:10:40 +0200

    fixes #45 and #42, I think

--- src/translate-selection.js
+++ src/translate-selection.js
@@ -1,5 +1,13 @@
-export default function(xy) {
-  return this.attr('transform', function(d,i) {
-    return 'translate('+[typeof xy == 'function' ? xy.call(this, d,i) : xy]+')';
-  });
+export default function(xy, dim) {
+  return this.node().getBBox ?
+    this.attr('transform', function(d,i) {
+      var p = [typeof xy == 'function' ? xy.call(this, d,i) : xy];
+      if (dim === 0) p = p[p, 0]); else if (dim === 1) p = [0, p];
+      return 'translate(' + p[0] +','+ p[1]+')';
+    }) :
+    this.style('transform', function(d,i) {
+      var p = [typeof xy == 'function' ? xy.call(this, d,i) : xy];
+      if (dim === 0) p = p[p, 0]); else if (dim === 1) p = [0, p];
+      return 'translate(' + p[0] +'px,'+ p[1]+'px)';
+    });
 }
