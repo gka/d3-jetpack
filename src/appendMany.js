74c372158ebfc13c1f306fe731f3b4558747ae45
Adam Pearce <1wheel@gmail.com>
2017-09-27 23:49:43 -0400

    removes logging

--- src/appendMany.js
+++ src/appendMany.js
@@ -1,13 +1,10 @@
 export default function(name, data){
-  console.log(name, data)
   if (typeof(data) == 'string'){
     console.warn("DEPRECATED: jetpack's appendMany order of arguments has changed. It's appendMany('div', data) from now on")
     var temp = data
     data = name
     name = temp
   }
-
-  console.log(name, data)
 
   return this.selectAll(null).data(data).enter().append(name);
 }
