a05650621d2b1af78cdc13d5900782e9f063df23
Gregor Aisch <gka@users.noreply.github.com>
2020-11-09 13:48:40 +0100

    (optionally) expose d3-selection methods directly (fixes #68)

---
+++ selection.js
@@ -0,0 +1,27 @@
+import './essentials';
+
+import {
+    create,
+    creator,
+    matcher,
+    select,
+    selectAll,
+    selection,
+    selector,
+    selectorAll,
+    style,
+    window,
+} from "d3-selection";
+
+export {
+    create,
+    creator,
+    matcher,
+    select,
+    selectAll,
+    selection,
+    selector,
+    selectorAll,
+    style,
+    window,
+};
