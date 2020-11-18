adf61754db79606415c88bcfae8a33977ee91fb8
Gregor Aisch <gka@users.noreply.github.com>
2020-11-18 22:02:47 +0100

    pass more useful selection info to tspans function

--- src/tspans.js
+++ src/tspans.js
@@ -1,7 +1,7 @@
 export default function(lines, lh) {
   return this.selectAll('tspan')
-      .data(function(d) {
-        return (typeof(lines) == 'function' ? lines(d) : lines)
+      .data(function(d, i) {
+        return (typeof(lines) === 'function' ? lines.call(this, d, i) : lines)
           .map(function(l) {
             return { line: l, parent: d };
           });
@@ -10,5 +10,5 @@ export default function(lines, lh) {
     .append('tspan')
       .text(function(d) { return d.line; })
       .attr('x', 0)
-      .attr('dy', function(d, i) { return i ? (typeof(lh) == 'function' ? lh(d.parent, d.line, i) : lh) || 15 : 0; });
+      .attr('dy', function(d, i) { return i ? (typeof(lh) === 'function' ? lh.call(this, d.parent, d.line, i) : lh) || 15 : 0; });
 }
