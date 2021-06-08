4dbec980c2ed64829064fc4c119515e058974e9d
Adam Pearce <adampearce@google.com>
2021-06-08 14:47:02 -0400

    starts ts

---
+++ test/test.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+// jp.cat()
+// console.log(jp);
+var str = 'Adam';
+var me = str.TOUPDATE();
---
+++ test/test.ts
@@ -0,0 +1,8 @@
+// import * as jp from '../index.js'
+import * as jp from '../build/d3-jetpack.js'
+// jp.cat()
+// console.log(jp);
+
+
+const str = 'Adam'
+let me = str.TOUPDATE()
