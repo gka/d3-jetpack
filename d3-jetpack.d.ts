dba80d32f9492cfbeb00e067b13444edd8a812a2
Adam Pearce <adampearce@google.com>
2021-06-08 23:24:26 -0400

    adds def for round

---
+++ d3-jetpack.d.ts
@@ -0,0 +1,16 @@
+// https://www.npmjs.com/package/@types/d3
+
+// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/d3-transition/index.d.ts
+
+// import { ArrayLike, BaseType, Selection, ValueFn } from 'd3-selection';
+
+
+declare module 'd3-jetpack'{
+  function round(n:number): number;
+
+
+  export {round}
+}
+
+
+
